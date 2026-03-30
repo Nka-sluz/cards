b23f4be9e2db7325c264ce1ba5ce40d1fc6c9a23
Nataliia Kakhnych <nataliia_kakhnych@sluz.ch>
2026-03-30 10:52:57 +0200

    Aufgabe 1

--- cards.ts
+++ cards.ts
@@ -1,3 +1,34 @@
-export function hello(): string {
-  return "world";
+export class Card {
+    constructor(public suit: Suit, public rank: Rank) {}
+
+    public beats(other: Card) {
+      if (this.rank > other.rank) {
+        return true;
+      }
+      else if (this.rank == other.rank) {
+        if (this.suit > other.suit) {
+          return true;
+        }
+      }
+      return false;
+    }
+}
+
+export enum Suit {
+    Rosen = 1,
+    Schellen = 2,
+    Schilten = 2,
+    Eicheln = 3
+}
+
+export enum Rank {
+    Sechs = 6,
+    Sieben = 7,
+    Acht = 8,
+    Neun = 9,
+    Banner = 10,
+    Under = 11,
+    Ober = 12,
+    König = 13,
+    Ass = 14
 }
--- cards_test.ts
+++ cards_test.ts
@@ -1,6 +1,50 @@
 import { assertEquals } from "@std/assert";
-import { hello } from "./cards.ts";
+import { Card, Rank, Suit } from "./cards.ts";
 
-Deno.test("hello world", () => {
-  assertEquals(hello(), "world");
+Deno.test("bigger card beats smaller card", () => {
+  //Arrange
+  const firstCard = new Card(Suit.Rosen, Rank.Sieben);
+  const secondCard = new Card(Suit.Schellen, Rank.Sechs);
+
+  //Act
+  const result = firstCard.beats(secondCard);
+
+  //Assert
+  assertEquals(result, true);
+});
+
+Deno.test("smaller card does not beat bigger card", () => {
+  //Arrange
+  const firstCard = new Card(Suit.Rosen, Rank.Sechs);
+  const secondCard = new Card(Suit.Schellen, Rank.Sieben);
+
+  //Act
+  const result = firstCard.beats(secondCard);
+
+  //Assert
+  assertEquals(result, false);
+});
+
+Deno.test("better suit card beats worse suit card", () => {
+  //Arrange
+  const firstCard = new Card(Suit.Schellen, Rank.Sechs);
+  const secondCard = new Card(Suit.Rosen, Rank.Sechs);
+
+  //Act
+  const result = firstCard.beats(secondCard);
+
+  //Assert
+  assertEquals(result, true);
+});
+
+Deno.test("worse suit card does not beat better suit card", () => {
+  //Arrange
+  const firstCard = new Card(Suit.Rosen, Rank.Sechs);
+  const secondCard = new Card(Suit.Schellen, Rank.Sechs);
+
+  //Act
+  const result = firstCard.beats(secondCard);
+
+  //Assert
+  assertEquals(result, false);
 });
